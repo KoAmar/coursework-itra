994d573793312116bae89b76675d855f0c530f94
KoAmar <pa1318vel@gmail.com>
2019-06-11 18:59:22 +0300

    material nav bar

--- course-front/src/app/app.module.ts
+++ course-front/src/app/app.module.ts
@@ -4,25 +4,41 @@ import {AppRoutingModule} from './app-routing.module';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
+  MatButtonToggleModule,
   MatButtonModule,
   MatToolbarModule,
   MatSidenavModule,
   MatIconModule,
   MatListModule,
-  MatMenuModule
+  MatMenuModule,
+  MatFormFieldModule
 } from '@angular/material';
 import {HomeComponent} from './home/home.component';
 
+import {DialogOverviewExample, DialogOverviewExampleDialog} from './input-dialog/input-dialog.component';
+
+const MaterialComponents = [
+  MatButtonToggleModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatFormFieldModule
+];
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DialogOverviewExample,
+    DialogOverviewExampleDialog
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatMenuModule
+    MaterialComponents
   ],
   providers: [],
   bootstrap: [HomeComponent]
